b6567512c48395d3e67aac4c23f8ff7dc8dfc9d4
alantea <alan094@gmail.com>
2021-08-29 21:40:40 +0800

    Update build config

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-    chainWebpack: config => {
-      config
-      .plugin('html')
-      .tap(args => {
-        args[0].title = '貓咪時程提醒表'
-        return args
-      })
-    }
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/battlecat-normal-event-generator/'
+    : '/',
+  productionSourceMap: false,
+  chainWebpack: config => {
+    config
+    .plugin('html')
+    .tap(args => {
+      args[0].title = '貓咪時程提醒表'
+      return args
+    })
   }
+}
